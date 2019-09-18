f173b80a721ef76721184693544c6d5f3aa3f4fc
Jeisson Guevara <guevara@civicplus.com>
2019-09-18 17:33:34 -0500

    add-api-security: Created Filter.

---
+++ Itof.UI/Filters/AvoidExternalRequestsAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Itof.UI.Filters
+{
+    public class AvoidExternalRequestsAttribute
+    {
+        public AvoidExternalRequestsAttribute()
+        {
+        }
+    }
+}
