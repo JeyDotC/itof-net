f65a674e6961ef9574a660844b4a4d302f20b8bc
Jeisson Guevara <guevara@civicplus.com>
2021-02-18 00:25:19 -0500

    add-basic-file-data: Made drive sizes human readable

--- Itof.UI/ClientApp/src/components/DriveTreeNode.js
+++ Itof.UI/ClientApp/src/components/DriveTreeNode.js
@@ -1,16 +1,20 @@
 ﻿import React, { Component, Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHdd } from '@fortawesome/free-solid-svg-icons'
+import humanFileSize from '../services/humanFileSize';
 
 export default class DriveTreeNode extends Component {
     static displayName = DriveTreeNode.name;
     
     render() {
+        const freeSpace = humanFileSize(this.props.drive.totalFreeSpace);
+        const totalSpace = humanFileSize(this.props.drive.totalSize);
+
         return (
             <Fragment>
                 <FontAwesomeIcon icon={faHdd} />
                 <strong> {this.props.drive.name}</strong> <br />
-                <small className="text-muted">{this.props.drive.totalFreeSpace} available of {this.props.drive.totalSize} </small>
+                <small className="text-muted">{freeSpace} available of {totalSpace} </small>
             </Fragment>
         );
     }
