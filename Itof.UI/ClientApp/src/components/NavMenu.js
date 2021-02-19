e4c37751e805a3b5201f1c1ba363f4e36b25e1bb
Jeisson Guevara <guevara@civicplus.com>
2021-02-19 18:09:24 -0500

    convert-to-hooks: Converted components/NavMenu.js

--- Itof.UI/ClientApp/src/components/NavMenu.js
+++ Itof.UI/ClientApp/src/components/NavMenu.js
@@ -1,79 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container, Navbar } from 'reactstrap';
 import DirectoryBar from './DirectoryBar';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
-    static displayName = NavMenu.name;
+export function NavMenu(props) {
 
-    constructor(props) {
-        super(props);
+    const { currentPath, onNavigate } = props;
 
-        this.toggleNavbar = this.toggleNavbar.bind(this);
-        this.state = {
-            collapsed: true,
-            editingPath: false,
-            pathBeingEdited: this.props.currentPath
-        };
-    }
+    const [collapsed, setCollapsed] = useState(true);
+    const [editingPath, setEditingPath] = useState(false);
+    const [pathBeingEdited, setPathBeingEdited] = useState(currentPath);
 
-    toggleNavbar() {
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
 
-    handleEditPath = () => {
-        this.setState({
-            editingPath: true,
-            pathBeingEdited: this.props.currentPath
-        });
-    }
+    const toggleNavbar = () => {
+        setCollapsed(!collapsed);
+    };
 
-    handleFinishEditPath = () => {
-        this.setState({
-            editingPath: false
-        });
-    }
+    const handleEditPath = () => {
+        setEditingPath(true);
+        setPathBeingEdited(currentPath);
+    };
 
-    handleKeyDown = e => {
+    const handleFinishEditPath = () => {
+        setEditingPath(false);
+    };
+
+    const handleKeyDown = e => {
         if (e.key === 'Enter') {
             e.preventDefault();
-            this.props.onNavigate(this.state.pathBeingEdited);
+            onNavigate(pathBeingEdited);
         }
         if (e.key === 'Escape') {
             e.preventDefault();
-            this.handleFinishEditPath();
+            handleFinishEditPath();
         }
+    };
+
+    const handlePathInputChanged = e => {
+        setPathBeingEdited(e.target.value);
     }
 
-    handlePathInputChanged = e => {
-        this.setState({
-            pathBeingEdited: e.target.value
-        });
-    }
-
-    handleFocus = e => {
+    const handleFocus = e => {
         const value = e.target.value;
         e.target.value = '';
         e.target.value = value;
     }
 
-    render() {
-        return (
-            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" fixed={"top"} light={true}>
-                <Container className="border" onClick={this.handleEditPath}>
-                    {!this.state.editingPath && <DirectoryBar className="navbar-left" currentPath={this.props.currentPath || ''} onNavigate={this.props.onNavigate} />}
-                    {this.state.editingPath && <input ref={input => input && input.focus()}
-                        className="form-control"
-                        type="text"
-                        onChange={this.handlePathInputChanged}
-                        onKeyDown={this.handleKeyDown}
-                        onBlur={this.handleFinishEditPath}
-                        onFocus={this.handleFocus}
-                        value={this.state.pathBeingEdited} />}
-                </Container>
-            </Navbar>
-        );
-    }
+    return (
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" fixed={"top"} light={true}>
+            <Container className="border" onClick={handleEditPath}>
+                {!editingPath && <DirectoryBar className="navbar-left" currentPath={currentPath || ''} onNavigate={onNavigate} />}
+                {editingPath && <input ref={input => input && input.focus()}
+                    className="form-control"
+                    type="text"
+                    onChange={handlePathInputChanged}
+                    onKeyDown={handleKeyDown}
+                    onBlur={handleFinishEditPath}
+                    onFocus={handleFocus}
+                    value={pathBeingEdited} />}
+            </Container>
+        </Navbar>
+    );
 }
