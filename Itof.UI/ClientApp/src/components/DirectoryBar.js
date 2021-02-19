ffbfecb5ff5a04c1dc4d843f3d0fac5b37e1385c
Jeisson Guevara <guevara@civicplus.com>
2021-02-19 17:37:30 -0500

    convert-to-hooks: Converted components/DirectoryBar.js

--- Itof.UI/ClientApp/src/components/DirectoryBar.js
+++ Itof.UI/ClientApp/src/components/DirectoryBar.js
@@ -1,36 +1,35 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 import { ButtonGroup, Button } from 'reactstrap';
 import './NavMenu.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHdd } from '@fortawesome/free-solid-svg-icons'
 
-export default class DirectoryBar extends Component {
-    static displayName = DirectoryBar.name;
+export default function DirectoryBar(props) {
 
-    handlePartClicked = (path, e) => {
+    const { onNavigate, currentPath } = props;
+
+    const handlePartClicked = (path, e) => {
         e.stopPropagation();
-        this.props.onNavigate(path);
+        onNavigate(path);
     }
 
-    renderPart = ({ path, part }) => <Button key={path} color={'default'} onClick={e => this.handlePartClicked(path, e)}>{part} &rsaquo;</Button>;
+    const renderPart = ({ path, part }) => <Button key={path} color={'default'} onClick={e => handlePartClicked(path, e)}>{part} &rsaquo;</Button>;
 
-    render() {
-        const currentPath = this.props.currentPath;
-        const parts = [(currentPath.startsWith('/') ? '/' : ''), ...currentPath.split('/')].filter(part => part.length !== 0);
-        let root = parts.shift();
-        const Part = props => this.renderPart(props);
+    const parts = [(currentPath.startsWith('/') ? '/' : ''), ...currentPath.split('/')].filter(part => part.length !== 0);
+    let root = parts.shift();
+    const Part = props => renderPart(props);
 
-        return (
-            <ButtonGroup size={'sm'}>
-                <Part path={root} part={<React.Fragment>
-                    <FontAwesomeIcon icon={faHdd} /> {root || '/'}
-                </React.Fragment>} />
-                {parts.map(part => {
-                    let path = `${root}/${part}`;
-                    root = path;
-                    return <Part key={path} path={path} part={part} />
-                })}
-            </ButtonGroup>
-        );
-    }
+    return (
+        <ButtonGroup size={'sm'}>
+            <Part path={root} part={<React.Fragment>
+                <FontAwesomeIcon icon={faHdd} /> {root || '/'}
+            </React.Fragment>} />
+            {parts.map(part => {
+                let path = `${root}/${part}`;
+                root = path;
+                return <Part key={path} path={path} part={part} />
+            })}
+        </ButtonGroup>
+    );
+
 }
